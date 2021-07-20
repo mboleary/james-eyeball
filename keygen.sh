688eb5e24c35caf71fc7445cb547462c049534ec
Brady O'Leary <thebradyoleary@gmail.com>
2021-07-19 23:28:05 -0400

    Added Header Parsing

--- keygen.sh
+++ keygen.sh
@@ -2,4 +2,4 @@
 
 # Generates a key for the server and client to use
 
-dd if=/dev/urandom of=/dev/stdout bs=256 count=1 status=none | base64
+dd if=/dev/urandom of=/dev/stdout bs=32 count=1 status=none | base64
